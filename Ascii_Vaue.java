683724658ce1ab137da5d0d464c1a8036d040dce
Anuj Patel <89893953+anujpatel581@users.noreply.github.com>
2022-10-09 00:26:28 +0530

    Create Ascii_Vaue.java

---
+++ Ascii_Vaue.java
@@ -0,0 +1,10 @@
+public class AsciiValue{
+    public static void main(String[] args){
+        char word = 'd';
+        int Ascii = word;
+        int CastAscii = word;
+
+        System.out.println("the Ascii value: "+ Ascii);
+        System.out.println("the CastAscii value:  "+ CastAscii);
+    }
+}
