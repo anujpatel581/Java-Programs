84f62a5309b1cc31d191dc3cd8c92f6243068817
Anuj Patel <89893953+anujpatel581@users.noreply.github.com>
2022-10-09 00:25:01 +0530

    Create  Quotient_Remainder.java

---
+++ Quotient_Remainder.java
@@ -0,0 +1,13 @@
+public class QuetientRemainder {
+    public static void main(String[] args){
+        int dividend = 25;
+        int divisor = 4;
+
+        int quetient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        System.out.println("quetient: " + quetient);
+        System.out.println("remainder: " + remainder);
+    }
+    
+}
